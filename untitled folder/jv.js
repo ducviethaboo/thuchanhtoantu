ed40ee58cfaeef6020d8c5c4df031bcbc4ba4ec6
tibeohaboo <macos@MACs-MacBook-Pro.local>
2020-09-10 14:34:29 +0700

    Add folder baitap

---
+++ untitled folder/jv.js
@@ -0,0 +1,7 @@
+
+let netwidth = prompt("Nhap chieu dai");
+let netheight = prompt("Nhap chieu rong");
+let width = parseInt(netwidth);
+let height = parseInt(netheight);
+let area = width*height;
+ document.write("dien tich=" + area )
